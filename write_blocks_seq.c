cbce824e022c93315b05042374f2291fc1e6f5c4
Oyewole Ayodeji <oyewolea@cdf.toronto.edu>
2017-01-24 04:43:33 -0500

    Buffer full: write to .dat file

--- write_blocks_seq.c
+++ write_blocks_seq.c
@@ -5,7 +5,7 @@
 
 
 void csv_to_records(const char * csv_path, long block_size){
-	FILE * fp;
+	FILE *fp, *fp_write;
     char * line = NULL;
     size_t len;
     int lines_read = 0;
@@ -13,10 +13,14 @@ void csv_to_records(const char * csv_path, long block_size){
     int records_per_block = block_size / sizeof(Record);
     Record * records = malloc(records_per_block * sizeof(Record));
 
-    fp = fopen(csv_path, "r");
-    if (fp == NULL) {
-        fprintf(stderr,"Error opening file\n");
+    if (!(fp = fopen(csv_path, "r"))) {
+        fprintf(stderr,"Error opening csv file\n");
         exit(1);
+    }
+
+	if (!(fp_write = fopen("records.dat", "wb"))) {
+		fprintf(stderr,"Error opening .dat file\n");
+		exit(1);
     }
 
     while (getline(&line, &len, fp) != -1) {
@@ -25,13 +29,22 @@ void csv_to_records(const char * csv_path, long block_size){
         
         lines_read++;
         if (lines_read >= records_per_block) {
-			/* appends the full page of serialized records to a binary file */
+			break;
 		}
     }
+	
+	fwrite(records, sizeof(Record), records_per_block, fp_write);
+	fflush(fp_write);
 
+
+	fclose(fp_write);
     fclose(fp);
+    
     if (line)
         free(line);
+    
+    if (records)
+        free(records);
 }
 
 int main( int argc, const char* argv[] )
