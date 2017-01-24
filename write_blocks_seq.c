3439c42f08ebe5432a9b598a6cd4316db74a188b
Ayodeji Oyewole <ayodeji.oyewole@gmail.com>
2017-01-24 06:32:26 -0500

    Fixed write timer

--- write_blocks_seq.c
+++ write_blocks_seq.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/timeb.h>
 #include "write_blocks_seq.h"
 
 
+/*
+ * Now it is time to implement a script in a scripting language of your choice (Python, bash),
+ * which runs write_blocks_seq with different block size parameters, and outputs the processing
+ * rate in Bytes Per Second (BPS) to stdout (can be redirected to a file). */
 
 void csv_to_records(const char * csv_path, long block_size){
 	FILE *fp, *fp_write;
-    char * line = NULL;
+    char line[MAX_CHARS_PER_LINE];
     size_t len;
     int lines_read = 0;
+    struct timeb t_begin, t_end;
+    long time_spent_ms;
     
     int records_per_block = block_size / sizeof(Record);
     Record * records = malloc(records_per_block * sizeof(Record));
@@ -23,26 +31,35 @@ void csv_to_records(const char * csv_path, long block_size){
 		exit(1);
     }
 
-    while (getline(&line, &len, fp) != -1) {
-        records[lines_read].uid1 = strtok(line, ' ');
-        records[lines_read].uid2 = strtok(NULL, ' ');
+	/* Start recording total write time */
+	ftime(&t_begin);
+
+    while (fgets(line, MAX_CHARS_PER_LINE, fp) != NULL) {
+		line[strcspn(line, "\r\n")] = '\0';
+
+		/* Converting each csv line to struct Record instances */
+        records[lines_read].uid1 = atoi(strtok(line, " "));
+        records[lines_read].uid2 = atoi(strtok(NULL, " "));
         
         lines_read++;
         if (lines_read >= records_per_block) {
+			lines_read--;
 			break;
 		}
     }
-	
-	fwrite(records, sizeof(Record), records_per_block, fp_write);
-	fflush(fp_write);
 
+	/* Write full block buffer to binary file & flush to disk */
+	fwrite(records, sizeof(Record), lines_read, fp_write);
+	fflush(fp_write);
 
 	fclose(fp_write);
     fclose(fp);
     
-    if (line)
-        free(line);
-    
+    /* Stop recording total write time */
+    ftime(&t_end);
+	time_spent_ms = (long) (1000 *(t_end.time - t_begin.time) + (t_end.millitm - t_begin.millitm));
+	printf ("Data rate: %.3f MBPS\n", ((lines_read * sizeof(Record))/(float)time_spent_ms * 1000)/MB);
+
     if (records)
         free(records);
 }
--- write_blocks_seq.h
+++ write_blocks_seq.h
@@ -1,3 +1,7 @@
+#define KB 1024
+#define MB (1024 * 1024)
+#define MAX_CHARS_PER_LINE 22
+
 typedef struct record {
 	int uid1;
 	int uid2;
